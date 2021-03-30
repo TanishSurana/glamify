ae1acbcf35858477b614ca4356a5970defb83bd5
TanishSurana <39584250+TanishSurana@users.noreply.github.com>
2021-03-30 21:44:27 +0530

    nav update
    color to upper nav, and lower nav basics

--- static/script.js
+++ static/script.js
@@ -2,12 +2,15 @@ const navslide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.onright');
     const nav1 = document.querySelector('.rightnav');
+    const nav2 = document.querySelector('.menu');
     const navLinks = document.querySelectorAll('.onright li');
 
     // mobile toggle
     burger.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
         nav1.classList.toggle('nav-active');
+        nav2.classList.toggle('nav-active');
+
 
         //animation for links
         navLinks.forEach((link, index) => {
