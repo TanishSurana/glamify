3b3ebeac0a28327e595ff9712ed678f76d200881
TanishSurana <39584250+TanishSurana@users.noreply.github.com>
2021-03-30 19:12:42 +0530

    Top nav animations

--- static/script.js
+++ static/script.js
@@ -1,14 +1,32 @@
 const navslide = () => {
     const burger = document.querySelector('.burger');
-    console.log('working');
     const nav = document.querySelector('.onright');
     const nav1 = document.querySelector('.rightnav');
+    const navLinks = document.querySelectorAll('.onright li');
 
+    // mobile toggle
     burger.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
         nav1.classList.toggle('nav-active');
+
+        //animation for links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.5}s`;
+            }
+        });
+        // burger button animation
+        burger.classList.toggle("toggle");
+
     }
     );
+
+    
+
+
+
 }
 
 const app = () =>{
