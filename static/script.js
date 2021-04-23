ae129b4319bae12feda7c91af85c4314824e3910
TanishSurana <39584250+TanishSurana@users.noreply.github.com>
2021-04-23 14:09:23 +0530

    banner made
    need help in navbar

--- static/script.js
+++ static/script.js
@@ -26,10 +26,6 @@ const navslide = () => {
     }
     );
 
-    
-
-
-
 }
 
 const app = () =>{
@@ -37,3 +33,45 @@ const app = () =>{
 }
 
 app();
+
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+    }
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+    }
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slideimage");
+    var dots = document.getElementsByClassName("bannerbuttons");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("whitefill", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " whitefill";
+
+    if(slideIndex == 2){
+        document.querySelector(".imagelogo").style.backgroundColor = "rgba(0, 0, 0, 0.411)";
+    }
+    else{
+        document.querySelector(".imagelogo").style.backgroundColor = "rgba(0, 0, 0, 0)";
+    }
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    // banner slideshow
+
+
+})
